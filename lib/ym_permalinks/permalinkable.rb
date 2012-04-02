9f549fc998d83ee75bb3c7baccd1bac03b4713eb
Matt Atkins <matt@yoomee.com>
2012-04-02 10:53:05 +0100

    User permalink_path= to create permalink

--- lib/ym_permalinks/permalinkable.rb
+++ lib/ym_permalinks/permalinkable.rb
@@ -2,8 +2,15 @@ module YmPermalinks::Permalinkable
   def self.included(base)
     base.has_many :permalinks, :as => :resource, :autosave => true, :dependent => :destroy
     base.has_one :permalink, :as => :resource, :conditions => {:active => true}
-    base.accepts_nested_attributes_for :permalink
     base.before_validation :set_permalink_path
+  end
+  
+  def permalink_path
+    permalink.try(:path)
+  end
+  
+  def permalink_path=(val)
+    (self.permalink || self.build_permalink).path = val
   end
   
   private
