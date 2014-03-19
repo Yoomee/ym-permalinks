5afcbce4d339a8d87024a160080e2d5221acc369
Matt Atkins <matt@yoomee.com>
2014-03-19 13:07:00 +0000

    Cope with more than just naked permalinks

--- lib/ym_permalinks/permalinks_handler.rb
+++ lib/ym_permalinks/permalinks_handler.rb
@@ -8,13 +8,13 @@ class PermalinksHandler
     status, headers, body = @app.call(env)
     if headers['X-Cascade'] == 'pass'
       path_info = (env['PATH_INFO'] || env['REQUEST_URI'] || env['REQUEST_PATH'])
-      if path_info.match(/^\/[^\/]+$/)
-        path = path_info.sub(/^\//, '').split(/\?/).first
-        if permalink = Permalink.find_by_path(path)
-          env['PATH_INFO'] = env['REQUEST_URI'] = env['REQUEST_PATH'] = permalink.resource_path
-          env['ORIGINAL_PATH_INFO'] = path_info
-          return @app.call(env)
-        end
+      without_params = path_info.split(/\?/).first
+      path = without_params.split(/\//).reject(&:blank?).first
+      if permalink = Permalink.find_by_path(path)
+        new_path = without_params.sub("/#{path}",permalink.resource_path)
+        env['PATH_INFO'] = env['REQUEST_URI'] = env['REQUEST_PATH'] = new_path
+        env['ORIGINAL_PATH_INFO'] = path_info
+        return @app.call(env)
       end
     end
     [status, headers, body]
