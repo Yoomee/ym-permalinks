69a672c6da6d269c3a38816cde6bd776e34e37ef
Matt Atkins <matt@yoomee.com>
2014-06-06 12:52:00 +0100

    Redirect old permalinks to the new one

--- lib/ym_permalinks/permalinks_handler.rb
+++ lib/ym_permalinks/permalinks_handler.rb
@@ -11,10 +11,17 @@ class PermalinksHandler
       without_params = path_info.split(/\?/).first
       path = without_params.split(/\//).reject(&:blank?).first
       if permalink = Permalink.find_by_path(path)
-        new_path = without_params.sub("/#{path}",permalink.resource_path)
-        env['PATH_INFO'] = env['REQUEST_URI'] = env['REQUEST_PATH'] = new_path
-        env['ORIGINAL_PATH_INFO'] = path_info
-        return @app.call(env)
+        if !permalink.active? && active_permalink = permalink.resource.permalinks.where(:active => true).first
+          new_url = ActionDispatch::Request.new(env).url.sub("/#{path}", "/#{active_permalink.path}")
+          response = Rack::Response.new
+          response.redirect(new_url)
+          return response.finish
+        else
+          new_path = without_params.sub("/#{path}",permalink.resource_path)
+          env['PATH_INFO'] = env['REQUEST_URI'] = env['REQUEST_PATH'] = new_path
+          env['ORIGINAL_PATH_INFO'] = path_info
+          return @app.call(env)
+        end
       end
     end
     [status, headers, body]
