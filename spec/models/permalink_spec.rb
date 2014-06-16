62f1f7b3b1f619289fc9dbf680103df5ced64f54
Edward Andrews-Hodgson <edward@yoomee.com>
2014-06-16 13:54:21 +0100

    Update tests
    use expect instead of should

--- spec/models/permalink_spec.rb
+++ spec/models/permalink_spec.rb
@@ -1,29 +1,28 @@
-require 'spec_helper'  
+require 'spec_helper'
 
 describe Permalink do
-  
+
   subject { FactoryGirl.create(:permalink) }
-  
-  it { should belong_to(:resource) }
-  it { should validate_presence_of(:path) }
-  it { should validate_uniqueness_of(:path).case_insensitive }
-  
-  describe do  
-    
-    let(:permalink) { FactoryGirl.create(:permalink) } 
-    
-    it "is valid" do
-      permalink.should be_valid
+
+  it { is_expected.to belong_to(:resource) }
+  it { is_expected.to validate_presence_of(:path) }
+  it { is_expected.to validate_uniqueness_of(:path).case_insensitive }
+
+  describe do
+
+    let(:permalink) { FactoryGirl.create(:permalink) }
+
+    it 'is valid' do
+      expect(permalink).to be_valid
     end
-    
-    it "creates inactive permalink with old name" do
+
+    it 'creates inactive permalink with old name' do
       old_permalink_path = permalink.path
-      permalink.update_attribute(:path,'new-permalink-name')
+      permalink.update_attribute(:path, 'new-permalink-name')
       Permalink.find_by_path(old_permalink_path).active == false
       Permalink.find_by_path(old_permalink_path).resource == permalink.resource
     end
-    
+
   end
-  
-    
+
 end
