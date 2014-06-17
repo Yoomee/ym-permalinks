e2437ef7cbb123eadc2c07906814db10fc867457
Edward Andrews-Hodgson <edward@yoomee.com>
2014-06-17 14:20:45 +0100

    Move to a rails-4 dummy app and use ym_test for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,15 +6,15 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
-# require File.expand_path("../dummy/config/environment", __FILE__)
-
-require 'rspec/autorun'
+require File.expand_path("../../test/dummy/config/environment", __FILE__)
 
 require 'ym_test/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -25,11 +25,29 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
-  config.infer_spec_type_from_file_location!
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = 'random'
+
+  # Set up database_cleaner
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    if Capybara.current_driver == :rack_test
+      DatabaseCleaner.strategy = :transaction
+    else
+      DatabaseCleaner.strategy = :truncation
+    end
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
 end
--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'cccd2d0e95bda0d8342623b69cd8733998cf00548a2fcaaee65f4e2a3ca42438cdf34c4c444f64a8ad284bfb1c39ed145eb04da7b037e7dda8f151fc160c8b81'
